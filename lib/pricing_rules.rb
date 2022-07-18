6c49fa417c975c2a6c761a46558d1ddbc70e905c
Lautaro Larroucau <llarroucau@gmail.com>
2022-07-18 01:02:16 -0300

    refactor pricing rules

--- lib/pricing_rules.rb
+++ lib/pricing_rules.rb
@@ -1,30 +1,23 @@
 # frozen_string_literal: true
 
 class PricingRules
-  VALID_PRODUCTS = %w[GR1 SR1 CF1].freeze
+  DISCOUNTS_FOR_PRODUCTS = {
+    'GR1': :buy_1_get_1_free_price,
+    'SR1': :fixed_discount,
+    'CF1': :proportional_discount
+  }.freeze
 
   def apply(product, quantity)
-    return item_amount_price(product.price, quantity) unless VALID_PRODUCTS.include?(product.code)
+    return item_amount_price(product.price, quantity) unless applies_discount?(product.code)
 
-    case product.code
-    when 'GR1'
-      buy_1_get_1_free_price(product.price, quantity)
-    when 'SR1'
-      if quantity >= 3
-        fixed_discount(product.price, quantity)
-      else
-        item_amount_price(product.price, quantity)
-      end
-    when 'CF1'
-      if quantity >= 3
-        proportional_discount(product.price, quantity, 2 / 3.0)
-      else
-        item_amount_price(product.price, quantity)
-      end
-    end
+    send(DISCOUNTS_FOR_PRODUCTS[product.code.to_sym], product.price, quantity)
   end
 
   private
+
+  def applies_discount?(product_code)
+    DISCOUNTS_FOR_PRODUCTS.keys.include?(product_code.to_sym)
+  end
 
   def buy_1_get_1_free_price(product_price, quantity)
     return product_price if quantity == 1
@@ -35,10 +28,14 @@ class PricingRules
   end
 
   def fixed_discount(product_price, quantity, discount_amount = 0.5)
+    return item_amount_price(product_price, quantity) unless quantity >= 3
+
     item_amount_price(product_price - discount_amount, quantity)
   end
 
-  def proportional_discount(product_price, quantity, discount_amount)
+  def proportional_discount(product_price, quantity, discount_amount = 2 / 3.0)
+    return item_amount_price(product_price, quantity) unless quantity >= 3
+
     item_amount_price(product_price * discount_amount, quantity)
   end
 
