2c76c7d879a901d361b1ff30f42dc6b2aa45296a
Lautaro Larroucau <llarroucau@gmail.com>
2022-07-18 01:02:57 -0300

    update checkout specs

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/BlockLength
+require_relative '../lib/checkout'
+require_relative '../lib/pricing_rules'
+require_relative '../lib/product'
+
 describe Checkout do
   context 'calculating the total price of items' do
+    let(:pricing_rules) { PricingRules.new }
+    let(:co) { Checkout.new(pricing_rules) }
+    let(:gr1) { Product.new('GR1', 'Green tea', 3.11) }
+    let(:sr1) { Product.new('SR1', 'Strawberries', 5.00) }
+    let(:cf1) { Product.new('CF1', 'Coffee', 11.23) }
+
     it 'returns the total price for the given test data 1' do
-      co = Checkout.new(pricing_rules)
-      basket = %w[GR1 SR1 GR1 GR1 CF1]
+      basket = [gr1, sr1, gr1, gr1, cf1]
       basket.each do |item|
         co.scan(item)
       end
@@ -16,8 +24,7 @@ describe Checkout do
     end
 
     it 'returns the total price for the given test data 2' do
-      co = Checkout.new(pricing_rules)
-      basket = %w[GR1 GR1]
+      basket = [gr1, gr1]
       basket.each do |item|
         co.scan(item)
       end
@@ -28,8 +35,7 @@ describe Checkout do
     end
 
     it 'returns the total price for the given test data 3' do
-      co = Checkout.new(pricing_rules)
-      basket = %w[SR1 SR1 GR1 SR1]
+      basket = [sr1, sr1, gr1, sr1]
       basket.each do |item|
         co.scan(item)
       end
@@ -39,9 +45,8 @@ describe Checkout do
       expect(price).to eq('£16.61')
     end
 
-    it 'returns the total price for the given test data 3' do
-      co = Checkout.new(pricing_rules)
-      basket = %w[GR1 CF1 SR1 CF1 CF1]
+    it 'returns the total price for the given test data 4' do
+      basket = [gr1, cf1, sr1, cf1, cf1]
       basket.each do |item|
         co.scan(item)
       end
